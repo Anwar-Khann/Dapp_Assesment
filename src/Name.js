07fdb75d2091499d2565099dc295f3b7ab307f4f
Anwarr-khann <AnwarDev69@gmail.com>
2022-12-02 19:07:33 +0500

    first commit

---
+++ src/Name.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import React from 'react'
+
+export default function Name() {
+  return (
+    <div>
+        <button type="button" class="btn btn-outline-primary">Name</button>
+
+    </div>
+  )
+}
---
+++ src/setName.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function setName() {
+  return (
+    <div>
+        <button type="button" class="btn btn-outline-primary">setName</button>
+
+    </div>
+  )
+}
